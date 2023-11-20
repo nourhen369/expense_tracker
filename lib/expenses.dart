54176973c9d49728c8a20bf9c60767d74ea2e5e1
nourhen <nourhan.khechine@insat.ucar.tn>
2023-11-20 16:20:54 +0100

    dummy data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,12 +11,16 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpenseState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense{title}
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(children: [
         Text("The chart"),
-        Text("Chart"),
+        Text("Expenses list ... "),
       ]),
     );
   }
