7ac84ab2e6c311cb4605b3119bd0ddf85a4b1988
nourhen <nourhan.khechine@insat.ucar.tn>
2023-11-21 00:28:10 +0100

    enabled deleting elements

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -40,14 +40,38 @@ class _ExpenseState extends State<Expenses> {
     });
   }
 
-  void _removeExpense(Expense expense) {
-    setState(() {
+
+void _removeExpense(Expense expense) {
+
+final expenseIndex = _registeredExpenses.indexOf(expense);
+setState(() {
       _registeredExpenses.remove(expense);
     });
-  }
+ScaffoldMessenger.of(context).showSnackBar(
+  SnackBar(
+    duration: const Duration(seconds: 3),
+    content : const Text("Expense deleted!"),
+    action: SnackBarAction(
+      label : 'Undo',
+      onPressed: (){
+        setState(() {
+          _registeredExpenses.insert(expenseIndex, expense);
+        });
+      },
+}
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("No expenses found. Start adding some!")
+    );
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent =ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text("ExpenseTracker"), actions: [
         IconButton(
@@ -59,10 +83,7 @@ class _ExpenseState extends State<Expenses> {
         // Toolbar with the Add button
         const Text("The chart"),
         Expanded(
-            child: ExpensesList(
-                expenses: _registeredExpenses,
-                onRemoveExpense: _removeExpense)),
-      ]),
+            child: mainContent,
     );
   }
 }
