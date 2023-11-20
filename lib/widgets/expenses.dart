d0b0e679df9a27497bf4b5f46d18ccdad7a3dc7f
nourhen <nourhan.khechine@insat.ucar.tn>
2023-11-20 23:23:31 +0100

    enabled adding a new expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -25,10 +26,30 @@ class _ExpenseState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text("ExpenseTracker"), actions: [
+        IconButton(
+          onPressed: _openAddExpenseOverlay,
+          icon: const Icon(Icons.add),
+        ),
+      ]),
       body: Column(children: [
+        // Toolbar with the Add button
         const Text("The chart"),
         Expanded(child: ExpensesList(expenses: _registeredExpenses)),
       ]),
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -12,6 +13,6 @@ class ExpensesList extends StatelessWidget {
     // we need an automatically scrollable list
     return ListView.builder(
         itemCount: expenses.length,
-        itemBuilder: (ctx, index) => Text(expenses[index].title));
+        itemBuilder: (ctx, index) => ExpenseItem(expenses[index]));
   }
 }
